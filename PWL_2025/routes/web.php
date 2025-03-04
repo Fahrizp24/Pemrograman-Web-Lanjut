4fb42a2a9f450166dab6ded64ab247437f83d283
Fahrizp24 <Fpradian24@gmail.com>
2025-03-04 23:33:20 +0700

    p2_Praktikum 2

--- PWL_2025/routes/web.php
+++ PWL_2025/routes/web.php
@@ -56,3 +56,9 @@ Route::get('/user/{name?}', function ($name='john') {
 Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+    ]);
+    Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy']);
