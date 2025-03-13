23a91bf46348b9b684c2f2391bd47c61654b36de
Fahrizp24 <Fpradian24@gmail.com>
2025-03-13 22:14:28 +0700

    p4_praktikum 1

--- POS_2025/app/Http/Controllers/UserController.php
+++ POS_2025/app/Http/Controllers/UserController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function profile($id,$name){
-        return view('userprofile')
-        ->with('name',$name)
-        ->with('id',$id);
-        
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-3',
+        //     'nama' => 'pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data); // tambahkan data ke tabel m_user
+
+        $data=[
+            'level_id'=> 2,
+            'username'=>'manager_lima',
+            'nama'=>'manager 5',
+            'password'=>Hash::make('12345')
+        ];
+        UserModel::create($data);
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
