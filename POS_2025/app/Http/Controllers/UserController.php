037813a6eeed072b9497dc097a5b70cfdc2921f6
Fahrizp24 <Fpradian24@gmail.com>
2025-03-13 22:56:28 +0700

    p4_praktikum 2.3

--- POS_2025/app/Http/Controllers/UserController.php
+++ POS_2025/app/Http/Controllers/UserController.php
@@ -19,15 +19,22 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-        $data=[
-            'level_id'=> 2,
-            'username'=>'manager_lima',
-            'nama'=>'manager 5',
-            'password'=>Hash::make('12345')
-        ];
-        UserModel::create($data);
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $data=[
+        //     'level_id'=> 2,
+        //     'username'=>'manager_lima',
+        //     'nama'=>'manager 5',
+        //     'password'=>Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+
+        // $user = UserModel::findOr(20,['username','nama'],function(){
+        //     abort(404);
+        // }); 
+
+        // $user = UserModel::findOrFail(100);
+        // $user = UserModel::where('username','manager9')-> firstOrFail();
+        $user = UserModel::where('level_id','2')-> count();
+        // dd($user);
         return view('user', ['data' => $user]);
     }
 }
